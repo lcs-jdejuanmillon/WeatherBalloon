0db1034b901e09008fc0de8cda8f1778c896f9e5
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2021-12-11 09:50:01 -0500

    Initial Commit

---
+++ WeatherBalloon/WeatherBalloonApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherBalloonApp.swift
+//  WeatherBalloon
+//
+//  Created by Jacobo de Juan Millon on 2021-12-11.
+//
+
+import SwiftUI
+
+@main
+struct WeatherBalloonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
